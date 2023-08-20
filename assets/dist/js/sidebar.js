9db50579403c0bf52e0e4a2067465a93c9ef7af9
Marc <marc.linden@outlook.de>
2023-08-20 16:38:19 +0200

    bugfixes

--- assets/dist/js/sidebar.js
+++ assets/dist/js/sidebar.js
@@ -1,21 +1,14 @@
-/*function openNav() {
-    
-    document.getElementById("mySidepanel").style.width = "400px";
-  }
-  
-  // Set the width of the sidebar to 0 (hide it) 
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
-*/
-function openSidebar(buttonId) {
+function openProgrammingLanguages(buttonId) {
   var sidebar = document.getElementById("sidebar");
+
+  // Roadmap / Programming Languages and External Packages
   var python = document.getElementById("python");
   var go = document.getElementById("go");
   var tailwind = document.getElementById("tailwind");
   var react = document.getElementById("react");
   var javascript = document.getElementById("javascript");
   var node = document.getElementById("node");
+
 
   // Roadmap / Programming Languages and External Packages
   if (buttonId === "python") {
@@ -64,9 +57,56 @@ function openSidebar(buttonId) {
     javascript.style.display = "none";
     node.style.display = "block";
   }
+  
+  sidebar.classList.toggle("open-sidebar");
 
+}
 
-
+// Roadmap / Static Webpages
+function openStaticWebpages(buttonId) {
+  var sidebar = document.getElementById("sidebar");
+  var html = document.getElementById("html");
+  var css = document.getElementById("css");
+  if (buttonId === "html") {
+    html.style.display = "block";
+    css.style.display = "none";
+  } else if (buttonId === "css") {
+    html.style.display = "none";
+    css.style.display = "block";
+  }
 
   sidebar.classList.toggle("open-sidebar");
 }
+
+
+// Roadmap / Operating Systems
+function openOperatingSystems(buttonId) {
+  var sidebar = document.getElementById("sidebar");
+  var windows = document.getElementById("windows");
+  var ubuntu = document.getElementById("ubuntu");
+  var rhel = document.getElementById("rhel");
+
+  // Roadmap / Operating Systems
+  if (buttonId === "windows") {
+    windows.style.display = "block";
+    ubuntu.style.display = "none";
+    rhel.style.display = "none";
+  } else if (buttonId === "ubuntu") {
+    windows.style.display = "none";
+    ubuntu.style.display = "block";
+    rhel.style.display = "none";
+  } else if (buttonId === "rhel") {
+    windows.style.display = "none";
+    ubuntu.style.display = "none";
+    rhel.style.display = "block";
+  }
+
+  sidebar.classList.toggle("open-sidebar");
+
+}
+
+function closeSidebar() {
+  var sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("open-sidebar");
+}
+
