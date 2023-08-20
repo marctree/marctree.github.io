1412c224889730bcbafc2c66116e7ec3078d95e2
Marc <marc.linden@outlook.de>
2023-08-20 14:13:41 +0200

    add dynamic

--- assets/dist/js/sidebar.js
+++ assets/dist/js/sidebar.js
@@ -12,14 +12,61 @@ function openSidebar(buttonId) {
   var sidebar = document.getElementById("sidebar");
   var python = document.getElementById("python");
   var go = document.getElementById("go");
+  var tailwind = document.getElementById("tailwind");
+  var react = document.getElementById("react");
+  var javascript = document.getElementById("javascript");
+  var node = document.getElementById("node");
 
+  // Roadmap / Programming Languages and External Packages
   if (buttonId === "python") {
     python.style.display = "block";
     go.style.display = "none";
+    tailwind.style.display = "none";
+    react.style.display = "none";
+    javascript.style.display = "none";
+    node.style.display = "none";
   } else if (buttonId === "go") {
     python.style.display = "none";
     go.style.display = "block";
+    tailwind.style.display = "none";
+    react.style.display = "none";
+    javascript.style.display = "none";
+    node.style.display = "none";
+  } else if (buttonId === "tailwind") {
+    python.style.display = "none";
+    go.style.display = "none";
+    tailwind.style.display = "block";
+    react.style.display = "none";
+    javascript.style.display = "none";
+    node.style.display = "none";
   }
+  else if (buttonId === "react") {
+    python.style.display = "none";
+    go.style.display = "none";
+    tailwind.style.display = "none";
+    react.style.display = "block";
+    javascript.style.display = "none";
+    node.style.display = "none";
+  }
+  else if (buttonId === "javascript") {
+    python.style.display = "none";
+    go.style.display = "none";
+    tailwind.style.display = "none";
+    react.style.display = "none";
+    javascript.style.display = "block";
+    node.style.display = "none";
+  }
+  else if (buttonId === "node") {
+    python.style.display = "none";
+    go.style.display = "none";
+    tailwind.style.display = "none";
+    react.style.display = "none";
+    javascript.style.display = "none";
+    node.style.display = "block";
+  }
+
+
+
 
   sidebar.classList.toggle("open-sidebar");
 }
