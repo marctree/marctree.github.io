21a3bac523f559d02d6e875765869a7ee5a11107
Marc <marc.linden@outlook.de>
2023-08-21 15:38:35 +0200

    roadmap

--- assets/dist/js/interactiveRoadmap.js
+++ assets/dist/js/interactiveRoadmap.js
@@ -289,6 +289,334 @@ function openHosting(buttonId) {
     closeHosting();
 }
 
+/**
+ * Opens the setup based on the given button ID.
+ *
+ * @param {string} buttonId - The ID of the button that triggered the setup opening.
+ * @return {undefined} This function does not return a value.
+ */
+
+function openSetup(buttonId) {
+    var check = document.getElementsByClassName("open-sidebar");
+
+    // Roadmap / Programming Languages and External Packages
+    var loadbalancer = document.getElementById("loadbalancer");
+    var firewall = document.getElementById("firewall");
+    var apache = document.getElementById("apache");
+    var fproxy = document.getElementById("fproxy");
+    var rproxy = document.getElementById("rproxy");
+    var tomcat = document.getElementById("tomcat");
+    var nginx = document.getElementById("nginx");
+
+    switch (buttonId) {
+        case "loadbalancer":
+            loadbalancer.style.display = "block";
+            firewall.style.display = "none";
+            apache.style.display = "none";
+            fproxy.style.display = "none";
+            rproxy.style.display = "none";
+            tomcat.style.display = "none";
+            nginx.style.display = "none";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        case "firewall":
+            loadbalancer.style.display = "none";
+            firewall.style.display = "block";
+            apache.style.display = "none";
+            fproxy.style.display = "none";
+            rproxy.style.display = "none";
+            tomcat.style.display = "none";
+            nginx.style.display = "none";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        case "apache":
+            loadbalancer.style.display = "none";
+            firewall.style.display = "none";
+            apache.style.display = "block";
+            fproxy.style.display = "none";
+            rproxy.style.display = "none";
+            tomcat.style.display = "none";
+            nginx.style.display = "none";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        case "fproxy":
+            loadbalancer.style.display = "none";
+            firewall.style.display = "none";
+            apache.style.display = "none";
+            fproxy.style.display = "block";
+            rproxy.style.display = "none";
+            tomcat.style.display = "none";
+            nginx.style.display = "none";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        case "rproxy":
+            loadbalancer.style.display = "none";
+            firewall.style.display = "none";
+            apache.style.display = "none";
+            fproxy.style.display = "none";
+            rproxy.style.display = "block";
+            tomcat.style.display = "none";
+            nginx.style.display = "none";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        case "tomcat":
+            loadbalancer.style.display = "none";
+            firewall.style.display = "none";
+            apache.style.display = "none";
+            fproxy.style.display = "none";
+            rproxy.style.display = "none";
+            tomcat.style.display = "block";
+            nginx.style.display = "none";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        case "nginx":
+            loadbalancer.style.display = "none";
+            firewall.style.display = "none";
+            apache.style.display = "none";
+            fproxy.style.display = "none";
+            rproxy.style.display = "none";
+            tomcat.style.display = "none";
+            nginx.style.display = "block";
+            if (check.length === 1) {
+                closeSetup();
+            }
+            break;
+        default:
+            buttonId = null;
+    }
+    closeSetup();
+}
+
+/**
+ * Opens the containers based on the given button ID.
+ *
+ * @param {string} buttonId - The ID of the button that triggers the opening of containers.
+ * @return {void} This function does not return a value.
+ */
+
+function openContainers(buttonId) {
+    var check = document.getElementsByClassName("open-sidebar");
+
+    var docker = document.getElementById("docker");
+
+    if (buttonId === 'docker') {
+        docker.style.display = "block";
+    }
+
+    if (check.length === 1) {
+        closeContainers();
+    }
+
+    closeContainers();
+}
+
+/**
+ * Opens a cloud based on the given button ID.
+ *
+ * @param {string} buttonId - The ID of the button that triggered the cloud opening.
+ * @return {void} This function does not return a value.
+ */
+
+function openCloud(buttonId) {
+    var check = document.getElementsByClassName("open-sidebar");
+
+    var aws = document.getElementById("aws");
+    var googlecloud = document.getElementById("googlecloud");
+    var azure = document.getElementById("azure");
+    var alibaba = document.getElementById("alibaba");
+
+    switch (buttonId) {
+        case "aws":
+            aws.style.display = "block";
+            googlecloud.style.display = "none";
+            azure.style.display = "none";
+            alibaba.style.display = "none";
+            if (check.length === 1) {
+                closeCloud();
+            }
+            break;
+        case "googlecloud":
+            aws.style.display = "none";
+            googlecloud.style.display = "block";
+            azure.style.display = "none";
+            alibaba.style.display = "none";
+            if (check.length === 1) {
+                closeCloud();
+            }
+            break;
+        case "azure":
+            aws.style.display = "none";
+            googlecloud.style.display = "none";
+            azure.style.display = "block";
+            alibaba.style.display = "none";
+            if (check.length === 1) {
+                closeCloud();
+            }
+            break;
+        case "alibaba":
+            aws.style.display = "none";
+            googlecloud.style.display = "none";
+            azure.style.display = "none";
+            alibaba.style.display = "block";
+            if (check.length === 1) {
+                closeCloud();
+            }
+            break;
+        default:
+            buttonId = null;
+    }
+
+    closeCloud();
+}
+
+/**
+ * Opens the network based on the provided button ID.
+ *
+ * @param {string} buttonId - The ID of the button that triggered the network opening.
+ * @return {undefined} This function does not return a value.
+ */
+
+function openNetwork(buttonId) {
+    var check = document.getElementsByClassName("open-sidebar");
+
+    var ftp = document.getElementById("ftp");
+    var dns = document.getElementById("dns");
+    var http = document.getElementById("http");
+    var https = document.getElementById("https");
+    var ssl = document.getElementById("ssl");
+    var ssh = document.getElementById("ssh");
+
+    switch (buttonId) {
+        case "ftp":
+            ftp.style.display = "block";
+            dns.style.display = "none";
+            http.style.display = "none";
+            https.style.display = "none";
+            ssl.style.display = "none";
+            ssh.style.display = "none";
+            if (check.length === 1) {
+                closeNetwork();
+            }
+            break;
+        case "dns":
+            ftp.style.display = "none";
+            dns.style.display = "block";
+            http.style.display = "none";
+            https.style.display = "none";
+            ssl.style.display = "none";
+            ssh.style.display = "none";
+            if (check.length === 1) {
+                closeNetwork();
+            }
+            break;
+        case "http":
+            ftp.style.display = "none";
+            dns.style.display = "none";
+            http.style.display = "block";
+            https.style.display = "none";
+            ssl.style.display = "none";
+            ssh.style.display = "none";
+            if (check.length === 1) {
+                closeNetwork();
+            }
+            break;
+        case "https":
+            ftp.style.display = "none";
+            dns.style.display = "none";
+            http.style.display = "none";
+            https.style.display = "block";
+            ssl.style.display = "none";
+            ssh.style.display = "none";
+            if (check.length === 1) {
+                closeNetwork();
+            }
+            break;
+        case "ssl":
+            ftp.style.display = "none";
+            dns.style.display = "none";
+            http.style.display = "none";
+            https.style.display = "none";
+            ssl.style.display = "block";
+            ssh.style.display = "none";
+            if (check.length === 1) {
+                closeNetwork();
+            }
+            break;
+        case "ssh":
+            ftp.style.display = "none";
+            dns.style.display = "none";
+            http.style.display = "none";
+            https.style.display = "none";
+            ssl.style.display = "none";
+            ssh.style.display = "block";
+            if (check.length === 1) {
+                closeNetwork();
+            }
+            break;
+        default:
+            buttonId = null;
+    }
+
+    closeNetwork();
+}
+
+
+function openServerless(buttonId) {
+    var check = document.getElementsByClassName("open-sidebar");
+
+    var vercel = document.getElementById("vercel");
+    var cloudflare = document.getElementById("cloudflare");
+
+
+    switch (buttonId) {
+        case "vercel":
+            vercel.style.display = "block";
+            cloudflare.style.display = "none";
+            if (check.length === 1) {
+                closeServerless();
+            }
+            break;
+        case "cloudflare":
+            vercel.style.display = "none";
+            cloudflare.style.display = "block";
+            if (check.length === 1) {
+                closeServerless();
+            }
+            break;
+        default:
+            buttonId = null;
+    }
+
+    closeServerless();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -326,5 +654,30 @@ function closeVCS() {
 
 function closeHosting() {
     var sidebar = document.getElementById("hosting");
+    sidebar.classList.toggle("open-sidebar");
+}
+
+function closeSetup() {
+    var sidebar = document.getElementById("setup");
+    sidebar.classList.toggle("open-sidebar");
+}
+
+function closeContainers() {
+    var sidebar = document.getElementById("containers");
+    sidebar.classList.toggle("open-sidebar");
+}
+
+function closeCloud() {
+    var sidebar = document.getElementById("cloud");
+    sidebar.classList.toggle("open-sidebar");
+}
+
+function closeNetwork() {
+    var sidebar = document.getElementById("network");
+    sidebar.classList.toggle("open-sidebar");
+}
+
+function closeServerless() {
+    var sidebar = document.getElementById("serverless");
     sidebar.classList.toggle("open-sidebar");
 }
